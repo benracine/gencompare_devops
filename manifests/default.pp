a4f673cf047f2fafcc3c7ba9e3e36efdadabcbcd
Ben Racine <ben.racine@cornerstonenw.com>
2012-03-23 17:36:02 -0700

    testing with chef

--- manifests/default.pp
+++ manifests/default.pp
@@ -16,3 +16,24 @@ class apache {
 }
 include apache
 
+
+#include postgresql::server
+#include postgresql::python
+#include python::dev
+#class { "python::dev": version => "2.7" }
+#include python::venv
+#include python::gunicorn
+
+
+# setuptools ()
+# pip (dev: easy_install)
+# rabbitmq-server (dev: brew)
+#	- to start on login:
+#		mkdir -p ~/Library/LaunchAgents
+#    	cp /usr/local/Cellar/rabbitmq/2.7.1/com.rabbitmq.rabbitmq-server.plist ~/Library/LaunchAgents/
+#    	launchctl load -w ~/Library/LaunchAgents/com.rabbitmq.rabbitmq-server.plist
+#    - 
+
+
+# redis (dev: brew)
+# nginx (dev: brew)
